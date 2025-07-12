44d11890dfe2636b5c005b43713957e2d4c49acd
Leonardo Amaro <la95112@gmail.com>
2025-07-12 16:23:41 -0300

    correção import sobrando

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,8 +1,6 @@
 "use server";
 
 import { createClient } from "@/lib/supabaseServer";
-import getGeoLocation from "@/models/Geo";
-import getWather from "@/models/Weather";
 import { redirect } from "next/navigation";
 
 export interface ForgotPasswordState {
