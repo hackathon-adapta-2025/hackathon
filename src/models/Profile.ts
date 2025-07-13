51edd4a0e38dcdd55d7a8ceb44be87cf9e2b430b
Alex Szefezuk <alexszefezuk@hotmail.com>
2025-07-12 22:31:39 -0300

    feat: add updateProfile

---
+++ src/models/Profile.ts
@@ -0,0 +1,45 @@
+import prisma from '@/lib/prisma'
+import { Profile } from '@prisma/client'
+
+const updateProfile = async ({
+  id,
+  faceShape,
+  skinTone,
+  HairTexture,
+  HairColor,
+  SignalPresence,
+  FacialExpression,
+  FacialSymmetry,
+  PhotoLighting,
+  useOfMakeup,
+  VisibleAccessories,
+  BackgroundType,
+  ImageQuality,
+  colorPalatte
+}: Profile) => {
+  try {
+    const profile = await prisma.profile.update({
+      where: { userId: id },
+      data: {
+        faceShape,
+        skinTone,
+        HairTexture,
+        HairColor,
+        SignalPresence,
+        FacialExpression,
+        FacialSymmetry,
+        PhotoLighting,
+        useOfMakeup,
+        VisibleAccessories,
+        BackgroundType,
+        ImageQuality,
+        colorPalatte
+      }
+    })
+    return profile
+  } catch (error) {
+    console.error('Error updating profile:', error)
+  }
+}
+
+export { updateProfile }
